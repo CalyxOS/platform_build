caa55d426f7531fc34002b5a14beb961b2df9aa2
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Calyx build support
    * Based on "build: Add Lineage build support"
    
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include Lineage pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use lineage pathmap
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- core/config.mk
+++ core/config.mk
@@ -230,6 +230,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(CALYX_BUILD),)
+include vendor/calyx/config/BoardConfigCalyx.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1174,8 +1178,10 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+ifneq ($(CALYX_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/calyx/sepolicy/common/sepolicy.mk)
+endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
