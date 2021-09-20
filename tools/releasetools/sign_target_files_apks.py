39a8d994cf29a4c4f566bfdd741dabfda099ec73
Chirayu Desai <chirayudesai1@gmail.com>
2021-09-20 23:47:48 +0530

    releasetools: Add method to replace cert fingerprint
    * Some of the apps we bundle rely on a list of allowed
      packagename to fingerprint mapping stored in a xml
      file on system
    * Replace cert fingerprint with the new key
    * Should be safe to just loop this on all keys, replacing
      as needed.
    
    Change-Id: Iaac5e41305d831db8aa2f1cd2f55aab8b48be4a6

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -791,6 +791,39 @@ def ReplaceCerts(data):
   return data
 
 
+def ReplaceFingerprints(data):
+  """Replaces all the occurences of X.509 cert fingerprints with the new ones.
+
+  The mapping info is read from OPTIONS.key_map. Non-existent certificate will
+  be skipped. After the replacement, it additionally checks for duplicate
+  entries, which would otherwise fail the policy loading code in
+  frameworks/base/services/core/java/com/android/server/pm/SELinuxMMAC.java.
+
+  Args:
+    data: Input string that contains a set of X.509 certs.
+
+  Returns:
+    A string after the replacement.
+
+  Raises:
+    AssertionError: On finding duplicate entries.
+  """
+  for old, new in OPTIONS.key_map.items():
+    if OPTIONS.verbose:
+      print("    Replacing %s.x509.pem with %s.x509.pem" % (old, new))
+
+    old_fp = common.ExtractFingerprint(old + ".x509.pem")
+    new_fp = common.ExtractFingerprint(new + ".x509.pem")
+
+    (data, num) = re.subn(old_fp, new_fp, data, flags=re.IGNORECASE)
+
+    if OPTIONS.verbose:
+      print("    Replaced %d occurence(s) of %s.x509.pem's fingerprint with %s.x509.pem's fingerprint" % (
+          num, old, new))
+
+  return data
+
+
 def EditTags(tags):
   """Applies the edits to the tag string as specified in OPTIONS.tag_changes.
 
