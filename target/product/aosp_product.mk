4def6d6e5361297ae9edca85ece4a3de0d646708
Chirayu Desai <chirayudesai1@gmail.com>
2022-04-30 03:58:18 +0530

    Skip audio inclusion for calyx builds
    * We'll handle this entirely in vendor/calyx
    
    Change-Id: I9e7430d9e8fc4c2a14d1a2d03d922551c34ef498

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -19,7 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
+ifeq ($(CALYX_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
