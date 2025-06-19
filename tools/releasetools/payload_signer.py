d8bdff8ba343dd8ba56dd1037732008caa3fee3d
Tommy Webb <tommy@calyxinstitute.org>
2025-06-19 13:38:47 -0400

    releasetools: Add PKCS#11 signing support
    Signing scripts:
    * Allow adding arguments to all signing-related invocations of
      avbtool. `--extra_avbtool_signing_args '--signing_helper signer.sh'`
      for example tells avbtool to use signer.sh for custom signing.
      This argument prevents needing to specify a signing helper for each
      of avbtool's many situation-dependent arguments.
    * Add --pkcs11_config for PKCS#11-based APK signing. Uses Java's PKCS#11
      provider with the supplied config file. The presence of this flag is
      a hint to the rest of the process to treat private keys differently;
      for example, the KEY_DIR environment variable prefix is removed from
      the private key path, and a file extension is not added.
    
    SignApk:
    * Add `-keyStorePinFromEnv` to load the PIN for the PKCS#11 keystore
      from the given environment variable so that it does not need to be
      passed directly on the command line.
    
    Issue: calyxos#11
    Change-Id: I08f5fd3c87334d7bb0708fd3182d7cd690add741

--- tools/releasetools/payload_signer.py
+++ tools/releasetools/payload_signer.py
@@ -111,9 +111,11 @@ class PayloadSigner(object):
                           "-pkeyopt", "digest:sha256"]
       self.maximum_signature_size = self._GetMaximumSignatureSizeInBytes(
           signing_key)
+      self.package_key = None
     else:
       self.signer = payload_signer
       self.signer_args = payload_signer_args
+      self.package_key = package_key
       if payload_signer_maximum_signature_size:
         self.maximum_signature_size = int(
             payload_signer_maximum_signature_size)
@@ -171,6 +173,13 @@ class PayloadSigner(object):
     """Signs the given input file. Returns the output filename."""
     out_file = common.MakeTempFile(prefix="signed-", suffix=".bin")
     cmd = [self.signer] + self.signer_args + ['-in', in_file, '-out', out_file]
+<<<<<<< PATCH SET (17d228 releasetools: Add PKCS#11 signing support)
+    if OPTIONS.pkcs11_config is not None and self.package_key is not None:
+      cmd.extend(["-inkey", self.package_key])
+    elif self.package_key is not None:
+      cmd.extend(["-inkey", self.package_key + private_key_suffix])
+=======
+>>>>>>> BASE      (bb482a releasetools: Add option to use a signing command intercepto)
     if OPTIONS.signing_command_interceptor is not None:
       new_env = os.environ.copy()
       new_env["SIGNING_COMMAND"] = cmd[0]
--- tools/signapk/src/com/android/signapk/SignApk.java
+++ tools/signapk/src/com/android/signapk/SignApk.java
@@ -1062,6 +1062,7 @@ class SignApk {
                            "[-providerArg <configureArg>] " +
                            "[-loadPrivateKeysFromKeyStore <keyStoreName>]" +
                            "[-keyStorePin <pin>]" +
+                           "[-keyStorePinFromEnv <environmentVariableName>] " +
                            "[--min-sdk-version <n>] " +
                            "[--disable-v2] " +
                            "[--enable-v4] " +
@@ -1123,6 +1124,13 @@ class SignApk {
                     usage();
                 }
                 keyStorePin = args[++argstart];
+                ++argstart;
+            } else if ("-keyStorePinFromEnv".equals(args[argstart])) {
+                // This is a bit less bad than providing the PIN directly on the command line.
+                if (argstart + 1 >= args.length) {
+                    usage();
+                }
+                keyStorePin = System.getenv(args[++argstart]);
                 ++argstart;
             } else if ("-a".equals(args[argstart])) {
                 alignment = Integer.parseInt(args[++argstart]);
