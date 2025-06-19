ba4495788a8e796a8d3703cc77fd563b224d32ca
Tommy Webb <tommy@calyxinstitute.org>
2025-06-19 13:38:47 -0400

    releasetools: Add PKCS#11 signing support
    Signing scripts:
    * Allow adding arguments to all signing-related invocations of
      avbtool. `--extra_avbtool_signing_args '--signing_helper signer.sh'`
      for example tells avbtool to use signer.sh for custom signing.
      This argument prevents needing to specify a signing helper for each
      of avbtool's many situation-dependent arguments.
    * Add --pkcs11_config for PKCS#11-based APK signing. Uses Java's PKCS#11
      provider with the supplied config file. The presence of this flag is
      a hint to the rest of the process to treat private keys differently;
      for example, the KEY_DIR environment variable prefix is removed from
      the private key path, and a file extension is not added.
    
    SignApk:
    * Add `-keyStorePinFromEnv` to load the PIN for the PKCS#11 keystore
      from the given environment variable so that it does not need to be
      passed directly on the command line.
    
    Issue: calyxos#11
    Change-Id: I08f5fd3c87334d7bb0708fd3182d7cd690add741

--- tools/releasetools/apex_utils.py
+++ tools/releasetools/apex_utils.py
@@ -152,6 +152,10 @@ class ApexApkSigner(object):
 
     if self.sign_tool:
       logger.info('Signing payload contents in apex %s with %s', self.apex_path, self.sign_tool)
+      if not signing_args:
+        signing_args = OPTIONS.extra_avbtool_signing_args
+      elif OPTIONS.extra_avbtool_signing_args:
+        signing_args = signing_args + " " + OPTIONS.extra_avbtool_signing_args
       # Pass avbtool to the custom signing tool
       if OPTIONS.signing_command_interceptor is not None:
         cmd = [self.sign_tool, '--avbtool', OPTIONS.signing_command_interceptor]
@@ -202,6 +206,10 @@ class ApexApkSigner(object):
     for key, val in arguments_dict.items():
       generate_image_cmd.extend(['--' + key, val])
 
+    if not signing_args:
+      signing_args = OPTIONS.extra_avbtool_signing_args
+    elif OPTIONS.extra_avbtool_signing_args:
+      signing_args = signing_args + " " + OPTIONS.extra_avbtool_signing_args
     if signing_args:
       generate_image_cmd.extend(
           ['--signing_args', signing_args])
@@ -241,6 +249,8 @@ def SignApexPayload(avbtool, payload_file, payload_key_path, payload_key_name,
     cmd.append('--no_hashtree')
   if signing_args:
     cmd.extend(shlex.split(signing_args))
+  if OPTIONS.extra_avbtool_signing_args:
+    cmd.extend(shlex.split(OPTIONS.extra_avbtool_signing_args))
 
   try:
     if OPTIONS.signing_command_interceptor is not None:
