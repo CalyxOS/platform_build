00daa67248a85f9d1ab257af7006ad247e9284e1
Tommy Webb <tommy@calyxinstitute.org>
2025-07-09 19:14:45 -0400

    DO NOT MERGE: DEBUGGING: NO_CLEANUP env variable
    Change-Id: Ie83ba6599c7d6170fdee467931144de82da7930e

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -2930,6 +2930,8 @@ def MakeTempDir(prefix='tmp', suffix=''):
 
 
 def Cleanup():
+  if os.getenv("NO_CLEANUP") == "y":
+    return None
   for i in OPTIONS.tempfiles:
     if not os.path.exists(i):
       continue
